7df68f13e6e92629f286eb4ae1b1ae39d572cc20
nustxujun@yahoo.com.cn <nustxujun@yahoo.com.cn@c2606ca0-2ebb-fda8-717c-293879e69bc3>
2010-09-28 08:01:46 +0000

    添加了逆矩阵计算

--- RendererComplement/include/Matrix4X4.h
+++ RendererComplement/include/Matrix4X4.h
@@ -164,7 +164,54 @@ namespace RCP
 			return kNeg;
 		}
 
+		inline bool isAffine(void) const
+        {
+            return m[3][0] == 0 && m[3][1] == 0 && m[3][2] == 0 && m[3][3] == 1;
+        }
 
+		Matrix4X4 Matrix4X4::inverseAffine(void) const
+		{
+			assert(isAffine());
+
+			float m10 = m[1][0], m11 = m[1][1], m12 = m[1][2];
+			float m20 = m[2][0], m21 = m[2][1], m22 = m[2][2];
+
+			float t00 = m22 * m11 - m21 * m12;
+			float t10 = m20 * m12 - m22 * m10;
+			float t20 = m21 * m10 - m20 * m11;
+
+			float m00 = m[0][0], m01 = m[0][1], m02 = m[0][2];
+
+			float invDet = 1 / (m00 * t00 + m01 * t10 + m02 * t20);
+
+			t00 *= invDet; t10 *= invDet; t20 *= invDet;
+
+			m00 *= invDet; m01 *= invDet; m02 *= invDet;
+
+			float r00 = t00;
+			float r01 = m02 * m21 - m01 * m22;
+			float r02 = m01 * m12 - m02 * m11;
+
+			float r10 = t10;
+			float r11 = m00 * m22 - m02 * m20;
+			float r12 = m02 * m10 - m00 * m12;
+
+			float r20 = t20;
+			float r21 = m01 * m20 - m00 * m21;
+			float r22 = m00 * m11 - m01 * m10;
+
+			float m03 = m[0][3], m13 = m[1][3], m23 = m[2][3];
+
+			float r03 = - (r00 * m03 + r01 * m13 + r02 * m23);
+			float r13 = - (r10 * m03 + r11 * m13 + r12 * m23);
+			float r23 = - (r20 * m03 + r21 * m13 + r22 * m23);
+
+			return Matrix4X4(
+				r00, r01, r02, r03,
+				r10, r11, r12, r13,
+				r20, r21, r22, r23,
+				0,   0,   0,   1);
+		}
 
 	};
 
